a9e78c52f1d76abf517e1cc53c13b1275493fb31
Samuel Ude <udesamuel88@gmail.com>
2025-04-18 02:21:16 +0200

    Updated with terraform code

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,7 @@
 # Create a VPC
-resource "aws_vpc" "pipeline-example-vpc" {
+resource "aws_vpc" "gitaction-pipeline-vpc" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "PipelineVPC"
+  }
 }
